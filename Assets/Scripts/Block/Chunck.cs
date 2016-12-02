760c125b146d7b501761776a665855ed878b2a7e
alepresciu <presciuttinia@gmail.com>
2016-12-02 13:53:10 +0100

    script and folder

---
+++ Assets/Scripts/Block/Chunck.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chunck : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+
+    void GetState()
+    {
+
+    }
+
+    void Move()
+    {
+
+    }
+
+    void Push()
+    {
+
+    }
+    void SetState()
+    {
+
+    }
+}
