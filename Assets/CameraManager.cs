df2a4edb21e053c83e41c2fea8fb2571f070f602
MarcoMoscati88 <marco.moscati88@gmail.com>
2016-12-05 14:38:49 +0100

    Camera v 1.0

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+	public float startFOV;
+	public float finalFOV;
+	public float speedFOV;
+
+	private float m_ftimer;
+	private float m_fruntimeFOV;
+
+	private void Awake()
+	{
+		Camera.main.fieldOfView = startFOV;
+		m_fruntimeFOV = startFOV;
+	}
+	private void Update()
+	{
+		if ( m_fruntimeFOV >= startFOV && m_fruntimeFOV <= finalFOV)
+		{
+			m_fruntimeFOV = m_fruntimeFOV + Mathf.Sin( m_ftimer * speedFOV );
+			Camera.main.fieldOfView = m_fruntimeFOV;
+			m_ftimer = +Time.deltaTime;
+		}
+	}
+}
